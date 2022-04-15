fc08709237f9aea81d62db3db54ce991efd49f2b
Pulkit Mahajan <2019ucs0073@iitjammu.ac.in>
2022-04-15 19:07:34 +0530

    corrected KMeans algorithm and added KMeans++ initialization, Pending: implement error metric inside the class

--- assignment1/KMeans.py
+++ assignment1/KMeans.py
@@ -8,19 +8,37 @@ import os
 # no need to implement normalization or standardization here itself. will 
 # put it in the main.py itself.
 class KMeansClustering:
-    def __init__(self, K, initializer = 'random'):
+    def __init__(self, K, initializer = 'KMeans++'):
         """
         in model.fit() give the dataframe object directly
         K (int)
         intializer = 'random' or 'KMeans++'
         """
         self.K = K
+        self.initr = initializer
 
 
     def random_initializer(self, shape):
         # shape should be a numpy array of shape [471, 6]
-        return np.random.rand(self.K,shape[1])
+        return 100*np.random.rand(self.K,shape[1])
         # returning K random centroids
+
+    def KMeansPlusPlus(self, X, K):
+        centroids = []
+        num_samples = X.shape[0]
+        features = X.shape[1]
+        ind = np.random.randint(0, num_samples, 1)
+        centroids.append(X[ind].flatten())
+        for i in range(self.K - 1):
+            distances = np.zeros(shape = [num_samples, len(centroids)])
+            for j in range(len(centroids)):
+                cen = np.array(centroids[i]).reshape(1, features)
+                distances[:,i] = np.sqrt(np.sum((X-cen)**2, axis = 1))
+            min_dist_from_nearest_cen = np.min(distances, axis = 1)
+            next_centroid = X[np.argmax(min_dist_from_nearest_cen)]
+            centroids.append(next_centroid)
+        
+        return np.array(centroids)
 
     def fit(self, X, max_iter = 20):
         """
@@ -30,31 +48,41 @@ class KMeansClustering:
             X = X.to_numpy()
         except Exception as e:
             print("could not convert input data to numpy array")
-        centroids = self.random_initializer(X.shape)
+
+        if(self.initr == 'KMeans++'):
+            centroids = self.KMeansPlusPlus(X, self.K)
+        else:
+            centroids = self.random_initializer(X.shape)
         class_index = None
         log_centroids = np.zeros(shape = [self.K, max_iter, X.shape[1]])
+        num_samples = X.shape[0]
+        features = X.shape[1]
         for iter in range(max_iter):
+            distances = np.zeros(shape = [num_samples, self.K])
+            for i in range(self.K):
+                cen = centroids[i].reshape(1,features)
+                distances[:,i] = np.sqrt(np.sum((X-cen)**2, axis=1))
 
-            distances = self.euc_dist(centroids, X)
             class_index = np.argmin(distances, axis = 1, keepdims=True)
+            # for j in range(self.K):
+            #     centroids[j] = np.sum(X*(class_index==j), axis= 0)/num_samples
+            #     log_centroids[j][iter] = centroids[j]
+            
+            
             for j in range(self.K):
-                centroids[j] = np.sum(X*(class_index==j), axis= 0)/X.shape[0]
+                num_samples_in_class_j = np.sum(class_index==j)
+                if(num_samples_in_class_j == 0):
+                    continue
+                      
+                centroids[j] = np.sum(X*(class_index==j), axis= 0)/num_samples_in_class_j
                 log_centroids[j][iter] = centroids[j]
 
         self.plot_transition(log_centroids)
+        print(centroids)
         return class_index 
 
-
-    def euc_dist(self, centroids, samples):
-        """
-        centroids: shape [K, 6]
-        dataset: shape [N, 6]
-        distances : shape [N, K]
-        """
-        dist_vec = np.zeros(shape = [self.K, samples.shape[0]])
-        for i in range(self.K):
-            dist_vec[i] = np.sqrt(np.sum(np.square(centroids[i] - samples), axis = 1)).reshape(1,samples.shape[0])     
-        return dist_vec.T
+    def sum_of_squared_errors(self,data, centroids, classes):
+        pass
         
     def plot_transition(self, log_centroids):
         fig, ax = plt.subplots(self.K, 1)
@@ -69,7 +97,7 @@ class KMeansClustering:
 
 
 if __name__ == "__main__":
-    KMeans = KMeansClustering(K = 3)
+    KMeans = KMeansClustering(K = 2)
     df = pd.read_csv('./data/covid_data_india.csv', index_col = None)
     df = df.iloc[:, 1:7]
     classes = KMeans.fit(df)
